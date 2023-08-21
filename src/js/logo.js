0de7e859f9a60dd3ade59e2ab70d018ce4ded180
Alexander Andhika Suryaperdana <frostgamers.fx@gmail.com>
2023-08-21 20:41:51 +0700

    chore: initial commit

---
+++ src/js/logo.js
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+import { keyframes } from "styled-components";
+
+export default function TextGradientComponent() {
+  return (
+    <>
+      <AnimatedGradientText>Hyperion Foundation CDN</AnimatedGradientText>
+    </>
+  );
+}
+
+const gradient = keyframes`
+{
+0% {
+  background-position: 0 50%;
+}
+50% {
+  background-position: 100% 50%;
+}
+100% {
+  background-position: 0 50%;
+}}
+`;
+const AnimatedGradientText = styled.span`
+  animation: ${gradient} 5s ease-in-out infinite;
+  background: linear-gradient(to right, #feefe2, #fee6d2, #fba254, #f98115);
+  background-size: 300%;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -webkit-background-clip: text;
+`;
---
+++ theme.config.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable import/no-anonymous-default-export */
+/* eslint-disable react-hooks/rules-of-hooks */
+import React from 'react';
+import { useRouter } from 'next/router';
+import { useConfig } from 'nextra-theme-docs';
+// import Script from 'next/script';
+import versionNumber from './package.json';
+import logo from './src/js/logo.js';
+
+export default {
+  logo: logo,
+  head: () => {
+    const { asPath } = useRouter();
+    const { frontMatter } = useConfig();
+
+    return <>
+      <meta property='og:url' content={`https://cdn.hyperfoundation.my.id${asPath}`} />
+      <meta property='og:title' content={frontMatter.title || 'Hyperion Foundation CDN'} />
+      <meta property='og:description' content={frontMatter.description || 'A Hyperion Foundation CDN website. Storing our old contents that can be used for public.' } />
+      <title>Hyperion Foundation CDN</title>
+      <link rel='icon' href='/hyperion-favicon.png' type='image/png' />
+    </>
+  },
+  i18n: [
+    { locale: 'en', text: '🇺🇸 English' },
+    { locale: 'id', text: '🇮🇩 Bahasa Indonesia' }
+  ],
+  // banner: {
+  //   key: 'Announcement',
+  //   text: (
+  //     'Some of the contents are not available, because they\'re not get backed up.'
+  //   ),
+  //   dismissible: false
+  // },
+  search: {
+    placeholder: 'Search something...'
+  },
+  project: {
+    link: 'https://github.com/hyperion-foundation/cdn'
+  },
+  chat: {
+    link: 'https://discord.gg/4U4k6bW'
+  },
+  sidebar: {
+    titleComponent({ title, type }) {
+      if (type === 'separator' ) {
+        return (
+          <div>{title}</div>
+        )
+      }
+      return <> {title}</>
+    },
+    defaultMenuCollapseLevel: 1
+  },
+  navigation: {
+    prev: true,
+    next: true
+  },
+  editLink: {
+    text: null
+  },
+  feedback: {
+    content: null
+  },
+  footer: {
+    text: <span>
+      &copy; {new Date().getFullYear()} <a href='https://www.hyperfoundation.my.id'>Hyperion Foundation</a> &mdash; {`v${versionNumber.version}`}
+    </span>
+  }
+}
